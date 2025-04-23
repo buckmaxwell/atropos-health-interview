30befe2353b6a27551463975a4d0344aa44745a4
Max Buck <maxbuckdeveloper@gmail.com>
2025-04-23 10:55:40 -0400

    Don't worry about redis clean up anymore in conftest

--- atropos/tests/conftest.py
+++ atropos/tests/conftest.py
@@ -1,7 +1,5 @@
-import os
 import pytest
 from fastapi.testclient import TestClient
-from redis import Redis
 
 from sqlmodel import Session as OrmSession
 from sqlmodel import SQLModel
@@ -42,26 +40,3 @@ def db_session():
         session.close()
         trans.rollback()
         connection.close()
-
-
-# Redis cleanup
-@pytest.fixture(autouse=True)
-def clear_redis_queue():
-    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    redis = Redis.from_url(redis_url)
-    redis.delete(TEST_TASK_QUEUE)
-    yield
-    redis.delete(TEST_TASK_QUEUE)
-
-
-def get_redis_client():
-    redis_url = os.getenv("REDIS_URL", "redis://localhost:6379/0")
-    return Redis.from_url(redis_url)
-
-
-@pytest.fixture(autouse=True)
-def clear_redis_queue():
-    redis = get_redis_client()
-    redis.delete(TEST_TASK_QUEUE)
-    yield
-    redis.delete(TEST_TASK_QUEUE)
